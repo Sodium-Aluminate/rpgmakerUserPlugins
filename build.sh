78a282e61e07fdee63731bb1d4ae0671f57b69db
Sodium-Aluminate <NaAlOH4@NaAlOH4.com>
2025-08-09 20:59:05 +0800

    build.sh: add comment flag for auto install script

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ for file in build/*.js; do
 done
 
 cat > manuallyInstall.js <<EOF
+//__RPGMAKER_USER_PLUGIN_REGISTER_START__
 // 安装方式：将此 js 文件（不是 sh 文件）复制到<游戏文件夹>/js/plugins.js 的结尾然后将 build 文件夹内的所有 js 复制到 js/plugins/ 文件夹
 // install guide: append this js file(not the sh file) to end of <game dir>/js/plugins.js, then copy all js file in build to <game dir>/js/plugins.
 ;(()=>{
@@ -24,4 +25,5 @@ $plugins\`.trim().split("\n");
 	for (const name of plugins) (name.startsWith("lib") ? libPlugins : normalPlugins).push(name);
 	[libPlugins, normalPlugins].flat().forEach(name => \$plugins.push({name, status: true, description: "", parameters: {}}))
 })();
+//__RPGMAKER_USER_PLUGIN_REGISTER_END__
 EOF
